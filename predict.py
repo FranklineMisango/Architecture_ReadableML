ca04d6cbdcd31ac0ca14cee5d3d70786fa0bcd64
misango <franklinemisango@gmail.com>
2024-03-12 16:18:13 +0800

    Prediction for an inhouse-image

---
+++ predict.py
@@ -0,0 +1,22 @@
+from roboflow import Roboflow
+import json 
+
+
+
+rf = Roboflow(api_key="Dnpd4PGIz0Yu8Y5lBgup")
+project = rf.workspace("franklines-space-ywbil").project("crf_testing")
+model = project.version(5).model
+image_location = "Test_Run_CRF/image/November_2023.png"
+
+# infer on a local image
+saved_json = model.predict(image_location, confidence=40, labels='draw').json()
+# Save the JSON object to a file
+with open('saved_prediction.json', 'w') as f:
+    json.dump(saved_json, f)
+
+# visualize your prediction
+model.predict(image_location, confidence=1).save("prediction.jpg")
+
+
+# infer on an image hosted elsewhere
+# print(model.predict("URL_OF_YOUR_IMAGE", hosted=True, confidence=40, overlap=30).json())
